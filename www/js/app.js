fd2403483e7e5c047239484a1a2635dc734c9c47
Curso HTML <Curso HTML>
2017-03-09 15:04:38 -0400

    inicio do projeto

---
+++ www/js/app.js
@@ -0,0 +1,34 @@
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    views: {
+      'view-inicial': {
+        templateUrl: 'template/login.html',
+        controller: 'dashCtrl'
+      }
+    }
+  });
+
+  
+  $urlRouterProvider.otherwise('/login');
+
+});
